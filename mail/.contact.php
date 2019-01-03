bfb9745ac4d26625d029a9b278b0f5d34ac81b36
Sahil Batla <sahilbathla1@gmail.com>
2019-01-04 02:33:59 +0530

    Add envelope from address

--- mail/.contact.php
+++ mail/.contact.php
@@ -1,8 +1,8 @@
 <?php
 header('Access-Control-Allow-Origin: *');
-if(empty($_POST['your_name'])  		||
-   empty($_POST['your_email']) 		||
-   empty($_POST['your_message'])	||
+if(empty($_POST['your_name'])           ||
+   empty($_POST['your_email'])          ||
+   empty($_POST['your_message'])        ||
    !filter_var($_POST['your_email'], FILTER_VALIDATE_EMAIL))
    {
     echo "No arguments Provided!";
@@ -18,9 +18,9 @@ $message = $_POST['your_message'];
 $to = 'support@proapp.tech'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Website Contact Form:  $name";
 $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nMessage:\n$message";
-$headers = "From: support@proapp.tech\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$headers = "From: noreply@ec2-13-127-219-160.ap-south-1.compute.amazonaws.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address" . "\r\n";
 $headers .= 'Cc: sahilbathla1@gmail.com' . "\r\n";
-mail($to,$email_subject,$email_body,$headers);
+mail($to,$email_subject,$email_body,$headers,'-f noreply@ec2-13-127-219-160.ap-south-1.compute.amazonaws.com');
 return true;
 ?>
