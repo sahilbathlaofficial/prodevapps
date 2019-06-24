cab727c5490217673a42f113d868d77e8d4e621d
mohdayaz <ayansaifi970@gmail.com>
2019-06-24 10:53:21 +0530

    modify portfolio images

--- js/career.js
+++
@@ -1,8 +0,0 @@
-$("#btn-career").click(function() {
-    $("#career").css('display', 'unset');
-    $("body").css('overflow','hidden');
-});
-$("#career-close").click(function(){
-    $("#career").css('display', 'none');
-    $("body").css('overflow','unset');
-});
